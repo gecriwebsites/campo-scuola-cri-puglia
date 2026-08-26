bdb6643dd4a02e049c689ce7317dd76a598611d7
Ge.CRI <admin@gecri.it>
2026-08-26 22:36:41 +0200

    Carica accredito semplificato V5

--- assets/js/config.js
+++ assets/js/config.js
@@ -79,7 +79,8 @@ window.CAMPO_CONFIG = {
     'assets/js/ux-segreteria-v3.js',
     'assets/js/accreditamento-verifica-editor.js',
     'assets/js/accreditamento-verifica-nav-fix.js',
-    'assets/js/accreditamento-wizard-v4.js'
+    'assets/js/accreditamento-wizard-v4.js',
+    'assets/js/accreditamento-semplice-v5.js'
   ];
 
   if (document.readyState === 'loading') {
